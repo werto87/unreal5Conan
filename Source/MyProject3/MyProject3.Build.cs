cfe8460d1a72fd8732af0397f13d9818ffd90f76
werto87 <huhulasmichinruhe@web.de>
2023-01-22 19:46:00 +0900

    enable exceptions

--- Source/MyProject3/MyProject3.Build.cs
+++ Source/MyProject3/MyProject3.Build.cs
@@ -1,27 +1,22 @@
 // Copyright Epic Games, Inc. All Rights Reserved.
 
-using UnrealBuildTool;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
 using System.Reflection;
+using UnrealBuildTool;
 
 public class MyProject3 : ModuleRules
 {
 	public MyProject3(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-		PublicDependencyModuleNames.AddRange(new string[] {"MyBoilerplateModule", "Core", "CoreUObject", "Engine", "InputCore" });
-		PrivateDependencyModuleNames.AddRange(new string[] {  });
-        Type type = Target.GetType();
-        FieldInfo innerField = type.GetField("Inner", BindingFlags.Instance | BindingFlags.NonPublic);
-        TargetRules inner = (TargetRules)innerField.GetValue(Target);
-		inner.AdditionalCompilerArguments += "-DPATH_TO_USER_DEFINED_GAME_OPTION=<modern_durak_game_option/gameOption.hxx>";
-		inner.bEnableCppCoroutinesForEvaluation=true;
+		bEnableExceptions = true;
+		PublicDependencyModuleNames.AddRange(new[]
+			{ "MyBoilerplateModule", "Core", "CoreUObject", "Engine", "InputCore" });
+		PrivateDependencyModuleNames.AddRange(new string[] { });
+		var type = Target.GetType();
+		var innerField = type.GetField("Inner", BindingFlags.Instance | BindingFlags.NonPublic);
+		var inner = (TargetRules)innerField.GetValue(Target);
+		inner.AdditionalCompilerArguments +=
+			"-DPATH_TO_USER_DEFINED_GAME_OPTION=<modern_durak_game_option/gameOption.hxx>";
+		inner.bEnableCppCoroutinesForEvaluation = true;
 	}
-
-
-	
-	
 }
